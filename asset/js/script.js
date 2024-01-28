6a5eb07dfdebc8eab82778e0c796861c6c08852c
Ibnu030303 <ibunu.nurdiyansa28@gmail.com>
2024-01-28 15:02:15 +0700

    web

---
+++ asset/js/script.js
@@ -0,0 +1,39 @@
+var navLinks = document.querySelectorAll('.nav-link');
+
+window.addEventListener('scroll', function() {
+    var currentScrollPos = this.window.scrollY;
+
+    this.document.querySelectorAll('section').forEach(function(section, index) {
+        var sectionTop = section.offsetTop - 72;
+        var sectionButtom = sectionTop + section.offsetHeight;
+
+        if (currentScrollPos >= sectionTop && currentScrollPos < sectionButtom) {
+            navLinks.forEach(function(link) {
+                link.classList.remove('active');
+            });
+
+            navLinks[index].classList.add('active');
+        }
+    });
+});
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    responsiveClass: true,
+    responsive: {
+        0:{
+            items: 1,
+        },
+        768:{
+            items: 2,
+        },
+        1100:{
+            items: 3,
+        },
+        1400:{
+            items: 4,
+            loop: false,
+        }
+    }
+});
